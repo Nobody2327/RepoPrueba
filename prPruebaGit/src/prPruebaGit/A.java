f62db6e52075b2834f7ce75b2f344fd852ad9b91
34610 <34610@DESKTOP-S7OUFL1.home>
2021-05-05 19:12:42 +0200

    Añado repo

---
+++ prPruebaGit/src/prPruebaGit/A.java
@@ -0,0 +1,10 @@
+package prPruebaGit;
+
+public class A {
+
+	public static void main(String[] args) {
+		System.out.println("Hola");
+
+	}
+
+}
---
+++ prPruebaGit/src/prPruebaGit/B.java
@@ -0,0 +1,11 @@
+package prPruebaGit;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Adios");
+
+	}
+
+}
